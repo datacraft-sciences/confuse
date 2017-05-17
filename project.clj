38be7086a4077b7b394b638ea6c81ecb5f6b8008
Mike <mike@mikera.net>
2017-05-17 12:30:16 +0800

    update core.matrix dependency

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
                  [com.rpl/specter "1.0.1"]
-                 [net.mikera/core.matrix "0.57.0"] ]
+                 [net.mikera/core.matrix "0.60.2"] ]
   :profiles  {:dev  {:dependencies  [[org.clojure/test.check "0.9.0"]]}
               :test {:dependencies  [[citius "0.2.4"]]}
               }
