2a8341fc1de9b631ad932498f50ee5e43eb38543
Kiran Karkera <kiran.karkera@gmail.com>
2017-12-31 16:22:24 +0800

    lein-release plugin: preparing 0.1.1 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject datacraft-sciences/confuse "0.1.1-SNAPSHOT"
+(defproject datacraft-sciences/confuse "0.1.1"
   :description "Utilities for Data Science"
   :url "https://github.com/datacraft-sciences/confuse"
   :license {:name "Eclipse Public License"
